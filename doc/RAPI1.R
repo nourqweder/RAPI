87efd94a0ed850d2105bbd4a84cae5be9d1143b8
Nour Elhouda Qweder <nouqw898@student.liu.se>
2019-10-05 23:02:48 +0200

    updtae API.rd

---
+++ doc/RAPI1.R
@@ -0,0 +1,11 @@
+## ---- include = FALSE----------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, include=FALSE------------------------------------------------
+library(RAPI)
+
+## ----installation, eval = FALSE------------------------------------------
+#  devtools::install_github("nourqweder/RAPI")
